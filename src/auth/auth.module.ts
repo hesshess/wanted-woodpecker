464fc720eb1295c1606c397256b7fd42c96ec6c2
Hess <hess90wang@gmail.com>
2024-10-04 22:51:33 +0900

    feat: jwt guard 적용

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,6 +5,8 @@ import { PassportModule } from '@nestjs/passport';
 import { KakaoStrategy } from './kakao.strategy';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.stratedy';
+import { JwtRefreshStrategy } from './jwt-refresh.stratedy';
 @Module({
   imports: [
     PassportModule.register({ defaultStrategy: 'kakao' }),    
@@ -14,6 +16,12 @@ import { JwtModule } from '@nestjs/jwt';
   }),
 ],
   controllers: [AuthController],
-  providers: [AuthService, KakaoStrategy, PrismaService],
+  providers: [
+    AuthService,
+    KakaoStrategy,
+    PrismaService,
+    JwtStrategy,
+    JwtRefreshStrategy,
+  ],
 })
 export class AuthModule {}
