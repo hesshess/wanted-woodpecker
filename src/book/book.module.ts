817962c088a5a5540605ba6f132d8525bbfb35cb
Hess <hess90wang@gmail.com>
2024-10-04 22:51:59 +0900

    feat: 책 공유 기능구현

--- src/book/book.module.ts
+++ src/book/book.module.ts
@@ -3,10 +3,11 @@ import { BookController } from './book.controller';
 import { BookService } from './book.service';
 import { HttpModule } from '@nestjs/axios';
 import { BookResolver } from './book.resolver';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   imports: [HttpModule],
   controllers: [BookController],
-  providers: [BookService, BookResolver],
+  providers: [BookService, BookResolver,JwtService],
 })
 export class BookModule {}
